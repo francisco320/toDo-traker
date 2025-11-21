a57f0e982d74c4454b38ae684cf8ceb38335a9c4
Francisco j Flores <Franciscojoseflores320@gmail.com>
2025-11-20 21:48:03 -0400

    fetch:nueva funcion para agregar task

--- app/app.js
+++ app/app.js
@@ -1,4 +1,24 @@
 import { openAndClose } from "./modal.js";
 
+const taskNew = document.getElementById("tarea");
+const dayTask = document.getElementById("date-js");
+const priority = document.getElementById("Prioridad");
+const btnAdd = document.querySelector(".addBtn");
+
+const task = [];
+const newTask = {
+  nameTask: taskNew.textContent,
+  date: dayTask.textContent,
+  priority: priority.textContent,
+  status: "pendiente",
+};
+function addNewTask() {
+  return task.push(newTask);
+}
+btnAdd.addEventListener("click", () => {
+  addNewTask();
+  console.log(task);
+});
+
 document.addEventListener("DOMContentLoaded", openAndClose);
 console.log("documento cargado");
